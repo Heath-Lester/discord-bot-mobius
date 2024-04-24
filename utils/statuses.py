63a649162ca1ee21d61cda07ca4b8b404505566d
Heath Lester <h.allen.lester@gmail.com>
2024-04-23 23:38:09 -0400

    fix statuses

--- utils/statuses.py
+++ utils/statuses.py
@@ -1,18 +1,23 @@
 """File containing bot statuses to loop through while active"""
 
 from discord import ActivityType
-
+"""_summary_
+    Status for the bot to display in the status column
+    NOTE: Bots cannot use custom statuses or unknown statuses
+"""
 STATUSES_WITH_TYPES: dict[str, ActivityType] = {
     "with explosives 🧶": ActivityType.playing,
     "with dolphins 🐬": ActivityType.playing,
     "by himself 🌒": ActivityType.playing,
     "with fish 🐠": ActivityType.playing,
-    "your mom 🙇‍♀️": ActivityType.playing,
+    "with your mom 🙇‍♀️": ActivityType.playing,
     "to your mom bitch 🤷‍♂️": ActivityType.listening,
     "Fight Club 🥊": ActivityType.watching,
+    "Pulp Fiction 💉": ActivityType.watching,
     "the world burn 🔥": ActivityType.watching,
-    "Is hungry 🎣": ActivityType.custom,
-    "Plotting his next move 💭": ActivityType.custom,
-    "Hates you 😠": ActivityType.custom,
-    "Needs to shit 💩": ActivityType.custom,
+    "fish 🐟": ActivityType.watching,
+    "for food 🦈": ActivityType.competing,
+    "conspiracy documentaries 🤔": ActivityType.watching,
+    "to bullshit 💩": ActivityType.listening,
+    "to System of a Down 🤘": ActivityType.listening,
 }
